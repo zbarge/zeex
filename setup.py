c53b38659b614e5b334cd021ef916b4c2063a173
Zeke <zekebarge@gmail.com>
2016-12-26 11:08:58 -0800

    Updated setup.py

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ class PyTest(TestCommand):
         sys.exit(errcode)
 
 tests_require = ['easygui', 'pandas >= 0.17.1', 'PySide', 'pytest',
-                            'pytest-cov', 'pytest-qt', 'python-magic==0.4.6']
+                            'pytest-cov', 'pytest-qt', 'python-magic==0.4.6', 'dropbox']
 setup(
     name='zeex',
     version=__version__,
@@ -65,7 +65,7 @@ setup(
     tests_require=tests_require,
     install_requires=['PySide','easygui', 'pandas>=0.17.1', 'pytest',
                       'pytest-qt>=1.2.2', 'pytest-cov', 'python-magic>=0.4.6',
-                      'openpyxl','xlrd','sqlalchemy'],
+                      'openpyxl','xlrd','sqlalchemy', 'dropbox'],
                       
     cmdclass={'test': PyTest},
     author_email='zekebarge@gmail.com',
